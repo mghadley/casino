e3e613f5fd83fb17f574a7b26cfb7a4c355dc8eb
Faith Oladele <faitholadele@gmail.com>
2016-05-16 14:35:24 -0600

    menu list

--- casino.rb
+++ casino.rb
@@ -4,8 +4,27 @@
 #high low game module or class
 require 'pry'
 
+#TODO create main method
+#player class
+#slot machine module or class
+#high low game module or class
+
 Dir[File.dirname(__FILE__) + '/mechanics/*.rb'].each {|file| require file}
 
-deck = Mechanics::Deck.new
-binding.pry
-puts deck
+class Casino
+
+	include Mechanics
+
+	def game_menu
+
+		Mechanics::Player.new
+		puts "***** select game *****"
+		puts "1. Slots"
+		puts "2. High/Low"
+		
+	end
+
+end
+
+@player_game = Casino.new
+@player_game.game_menu
