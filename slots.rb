e6c3feab08e3ba7134b19f835f6d799d17c9dee4
Brad Mulick <b.rad.mulick@gmail.com>
2016-05-16 15:42:36 -0600

    slots_build

---
+++ slots.rb
@@ -0,0 +1,16 @@
+class Slots
+	attr_accessor :player
+	
+
+	def play
+		puts "--Welcome to the $5 Slots---
+		Make a selection...
+		1)Spin
+		2)Cash Out and exit Slots"
+
+		sele = gets.to_i
+			if 1
+
+	end
+end		
+
