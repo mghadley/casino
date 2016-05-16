23ffb2fa1c6386edbb244952e87c8c115876ac88
Faith Oladele <faitholadele@gmail.com>
2016-05-16 14:25:26 -0600

    get menu runing

--- casino.rb
+++ casino.rb
@@ -2,3 +2,23 @@
 #player class
 #slot machine module or class
 #high low game module or class
+
+Dir[File.dirname(__FILE__) + '/mechanics/*.rb'].each {|file| require file}
+
+class Casino
+
+	include Mechanics
+
+	def game_menu
+
+		Mechanics::Player.new
+		puts "***** select game *****"
+		puts "1. Slots"
+		puts "2. High/Low"
+		
+	end
+
+end
+
+@player_game = Casino.new
+@player_game.game_menu
